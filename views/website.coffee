e1c5f3bc9a47757ac13a7ad3bec626e9fde6a4dd
yon <Lighthawky@gmail.com>
2011-11-29 22:46:38 +0200

    added remote and most of site work

--- views/website.coffee
+++ views/website.coffee
@@ -1,5 +1,5 @@
 Page = Backbone.Model.extend
-	initialize: (el,time, @interval = 10) ->
+	initialize: (el = $('.page') ,time = 5, @interval = 10) ->
 		@el = $(el)
 		@ui = 
 			progressBar: @el.find(".pb-size")
@@ -29,6 +29,7 @@ Page = Backbone.Model.extend
 	end: ->
 		clearInterval(@intervalObj)
 		console.log("END")
+		@playAlarm()
 	
 	update: ->
 		@updateProgressBar()		
@@ -83,6 +84,63 @@ Page = Backbone.Model.extend
 				next = $($('.text')[0]) if next.length == 0
 				next.fadeIn(1000)
 		)
+	playAlarm: ->
+		iframe = $($("#alarm").html())
+		iframe.attr("src","http://www.youtube.com/watch_popup?v=#{currentSession.alarm}&autoplay=1&iv_load_policy=3&version=3")
+		$('.text_area').empty().append(iframe)
+
+
+Raffle =  Backbone.Model.extend
+	initialize: (el = $(".raffle"), @timeToEnd=1000, @interval=50) ->
+		@el = $(el)
+		@start()
+
+	start: ->
+		if @el.find("li.won").length > 0
+			@el.find("li.won").fadeOut(600, => @el.find("li.won").remove();@startShuffling())
+		else
+			@startShuffling()
+
+	startShuffling: ->
+		console.log("Everyday I'm shuffling")
+		@el.find("li").removeClass("selected").removeClass("won").removeClass("lost")
+		setTimeout( 
+			=> @end()
+			@timeToEnd
+		)
+		@intervalEl = setInterval(
+			=> @tick()
+			@interval
+		)
+	tick: ->
+		newSelected = $(_.shuffle(@el.find("li:not(.selected)"))[0])
+		@el.find("li.selected").removeClass("selected")
+		newSelected.addClass("selected")
+
+	end: ->
+		@el.find("li:not(.selected)").addClass("lost")
+		@el.find("li.selected").removeClass("selected").addClass("won")
+		clearInterval(@intervalEl)
+		setTimeout(
+			=> @start()
+			2000
+			)
+
+	
 
 $().ready =>
-	new Page($('.page'),1)
+	if currentSession.type == "text"
+		d = $(_.template($("#text").html(), currentSession))
+		$(".text_area").append(d)
+		d.fadeIn()
+	else if currentSession.type == "session"
+		$('.session-counter').append($(_.template($("#session").html(), currentSession)))
+		ideasDiv = $(_.template($("#ideas").html(), currentSession))
+		$('.text_area').append(ideasDiv)
+		ideasDiv.show()
+		for page in currentSession.pages
+			$('.text_area').append($(_.template($("#text").html(), page)))
+		new Page($(".page"),5)
+	else if currentSession.type == "raffle"
+		$('.text_area').append($(_.template($("#raffle").html(), currentSession)))
+		new Raffle($('.raffle'))
--- web.rb
+++ web.rb
@@ -1,10 +1,91 @@
 require 'sinatra'
 require 'sass'
+require 'json'
 
+participants = [
+	"abyx",
+	"yon",
+	"erez",
+	"erik",
+	"yonix",
 
+	"roey",
+	"s_ageev",
+	"avihut",
+	"shmef",
+	"uri.s",
+
+	"iftach",
+	"3david",
+	"amirwilf",
+	"noamgat",
+	"itay",
+
+	"sheba",
+	"guy",
+	"yulkes", #
+	"alon",
+	"stanislav",
+
+	"danni",
+	"herve",
+	"yoavtz",
+	"ron",
+	"antmir"
+].shuffle()
+
+pages = [
+	{ :header => "Game of Life", :text => "rules are here blah blah <br> blah"},
+	{ :header => "S.O.L.I.D", :text => "Solid is good for you"}
+]
+
+ideas = [
+	["Regular","TDD"],
+	["No Primitives","Yes, And"],
+	["Polymorphism", "No mouse", "TDD as if"],
+	["Event Based", "3D World", "Evil mute programmer"],
+	["No Testing"],
+	["Infinite Universe"]
+]
+
+nyans = [
+	"QH2-TGUlwu4", #regular
+	"AaEmCFiNqP0", #jazz
+	"YsDab6JjXJg", #ascii
+
+	"OM-9Q0ac6Zs", #evil
+	"48GEvJLJoI4", #old
+	"EUgTHYFJfDM", #Jewish
+]
+
+sessions = [
+	{:type => 'text', :header => "Welcome", :text => "Enjoy Breakfast"},
+	{:type => 'session', :number => 1, :ideas => ideas[0],:alarm => nyans[0], :pages => pages},
+	{:type => 'session', :number => 2, :ideas => ideas[1],:alarm => nyans[1], :pages => pages},
+	{:type => 'session', :number => 3, :ideas => ideas[2],:alarm => nyans[2], :pages => pages},
+	{:type => 'text', :header => "Lunch Time", :text => "Om nom, nom, nom <div><img src='http://nyan-cat.com/images/nyan-cat.gif'></div>"},
+	{:type => 'session', :number => 4, :ideas => ideas[3],:alarm => nyans[3], :pages => pages},
+	{:type => 'session', :number => 5, :ideas => ideas[4],:alarm => nyans[4], :pages => pages},
+	{:type => 'session', :number => 6, :ideas => ideas[5],:alarm => nyans[5], :pages => pages},
+	{:type => 'raffle', :participants => participants},
+	{:type => 'text', :header => "Bye", :text => "Thanks for coming"}
+]
+
+current_session = 0
 
 get '/' do
- haml :website, :format => :html5
+	haml :website, :format => :html5, :locals => {:session => sessions[current_session]}
+end
+
+get '/current' do
+	content_type :json
+  	sessions[current_session].to_json
+end
+
+get '/next' do
+	current_session += 1
+	sessions[current_session]['startTime'] = Time.now if sessions[current_session][:type] == 'session'
+	haml :website, :format => :html5, :locals => {:session => sessions[current_session]}
 end
 
 get '/website.js' do
@@ -14,4 +95,23 @@ end
 get '/website.css' do
 	# header 'Content-Type' => 'text/css; charset=utf-8'
 	scss :website
+end
+
+get '/remote' do
+	st = sessions[current_session]['startTime']
+	if !st.nil?
+		@st = st
+		time_left = (st+45*60) - Time.now
+		@seconds_left = (time_left % 60).floor
+		@minutes_left = (time_left/60).floor
+	end
+	haml :remote, :format => :html5, :locals => {:session => sessions[current_session]}
+end
+
+get '/remote.js' do
+	coffee :remote
+end
+
+get '/remote.css' do
+	scss :remote
 end
