c1e6c0aa134834f2b82e44ae2f148d1dc27a408f
yon <Lighthawky@gmail.com>
2011-12-02 22:20:53 +0200

    imrpoved ideas

--- config/ideas.rb
+++ config/ideas.rb
@@ -1,8 +1,8 @@
 IDEAS = [
-	["Regular","TDD"],
-	["No Primitives","Yes, And"],
-	["Polymorphism", "No mouse", "TDD as if"],
-	["Event Based", "3D World", "Evil mute programmer"],
-	["No Testing"],
-	["Infinite Universe"]
+	["Nothing special - get to know to problem"],
+	["TDD", "No Primitives","Yes, And - Don't delete - only add","Polymorphism, don't use bools"],
+	["No Primitives","Polymorphism, don't use bools", "No mouse", "TDD as if you ment it"],
+	["TDD as if you ment it", "Event Based", "3D World", "Evil mute programmer"],
+	["No Testing","Infinite Universe", "TDD as if you ment it", "3D World", "No loops"],
+	["Make up your own"]
 ]
